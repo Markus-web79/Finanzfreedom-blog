c1ec54f3ef1c2c277339cc58074c3a1ef4f5b320
Markus-web79 <markushabermann@web.de>
2025-09-21 01:11:16 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let assetPrefix = '';
-let basePath = '';
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY?.split('/')[1];
-  basePath = `/${repo}`;
-  assetPrefix = `/${repo}/`;
-}
-
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  assetPrefix,
-  basePath,
-  distDir: 'out'
+  basePath: '/Finanzfreedom-blog',
+  assetPrefix: '/Finanzfreedom-blog/',
 };
 
 module.exports = nextConfig;

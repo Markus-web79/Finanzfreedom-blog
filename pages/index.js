5d14458ff6377d40c7a5b8c080358e6c00592bf9
Markus-web79 <markushabermann@web.de>
2025-09-20 18:36:59 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "sans-serif", padding: "2rem" }}>
+      <h1>FinanzFreedom Blog 🚀</h1>
+      <p>Willkommen auf deinem automatisch generierten Blog!</p>
+      <p>Die Artikel werden im Ordner <code>content/</code> gespeichert.</p>
+    </div>
+  );
+}

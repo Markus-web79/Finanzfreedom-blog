d020a2c296a3a5167d3712e03996266293f2c613
Markus-web79 <markushabermann@web.de>
2025-09-21 22:35:18 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let assetPrefix = '';
-let basePath = '';
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY?.split('/')[1];
-  basePath = `/${repo}`;
-  assetPrefix = `/${repo}/`;
-}
-
 const nextConfig = {
-  output: 'export',       // <--- wichtig für GitHub Pages
+  output: "export",
   images: { unoptimized: true },
-  assetPrefix,
-  basePath,
 };
 
 module.exports = nextConfig;

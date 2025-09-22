8addf810b5c9bc0b1c45a34e22f70ff8af3213b5
Markus-web79 <Markus-web79@users.noreply.github.com>
2025-09-22 09:54:15 +0000

    deploy: 77183b85bbc72a6d7a5bf024d2ab9d1b983278e5

--- _next/static/VuWrHEY-LzisvxZomrMGm/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-0023969c9e6aa973.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],"/pages/[slug]":["static/chunks/pages/pages/[slug]-dd81c4c1edc64f1f.js"],sortedPages:["/","/_app","/_error","/pages/[slug]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

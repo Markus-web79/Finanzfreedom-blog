eb9d64755b0651a33057ff4591e78632cfac8af7
Markus-web79 <markushabermann@web.de>
2025-09-22 12:12:55 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // sorgt dafür, dass Vercel saubere statische Seiten baut
   output: "export",
   images: { unoptimized: true },
+
+  // Rewrites: /pages/<slug> → /<slug>
+  async rewrites() {
+    return [
+      {
+        source: "/pages/:slug",
+        destination: "/:slug",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;

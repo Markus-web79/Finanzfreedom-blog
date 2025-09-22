f701958ad354e73ea2489a9a9d3cacd0b2e84de6
Markus-web79 <markushabermann@web.de>
2025-09-22 22:31:48 +0200

    Update _app.js

--- pages/pages/_app.js
+++ pages/pages/_app.js
@@ -1,17 +1,5 @@
-import "@/styles/globals.css";
-import Head from "next/head";
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        {/* Google Fonts einbinden */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Roboto+Slab:wght@700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }

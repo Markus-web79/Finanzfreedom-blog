f2e73e668d05d05e0e47ac7a6679a0865534911c
Markus-web79 <markushabermann@web.de>
2025-09-24 12:12:00 +0200

    Update checkContent.js

--- checkContent.js
+++ checkContent.js
@@ -1,49 +1,24 @@
-// checkContent.js
-import fs from "fs";
-import path from "path";
-
-const contentDir = path.join(process.cwd(), "content");
-
-// Wörterbuch für automatische Korrekturen
-const replacements = {
-  "Steür": "Steuer",
-  "steür": "steuer",
-  "Steürn": "Steuern",
-  "steürn": "steuern",
-  "ae": "ä",
-  "oe": "ö",
-  "ue": "ü",
-  "Ae": "Ä",
-  "Oe": "Ö",
-  "Ue": "Ü"
-};
-
-function fixContent(text) {
-  let fixed = text;
-  for (const [wrong, correct] of Object.entries(replacements)) {
-    const regex = new RegExp(wrong, "g");
-    fixed = fixed.replace(regex, correct);
+{
+  "name": "finanzfreedom-blog",
+  "version": "1.0.0",
+  "private": true,
+  "type": "module",
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start",
+    "lint": "next lint",
+    "check:content": "node checkContent.js"
+  },
+  "dependencies": {
+    "next": "14.2.5",
+    "react": "18.2.0",
+    "react-dom": "18.2.0"
+  },
+  "devDependencies": {
+    "eslint": "^8.56.0",
+    "eslint-config-next": "14.2.5"
   }
-  return fixed;
-}
-
-function checkFiles() {
-  const files = fs.readdirSync(contentDir);
-
-  files.forEach((file) => {
-    if (file.endsWith(".md")) {
-      const filePath = path.join(contentDir, file);
-      const content = fs.readFileSync(filePath, "utf8");
-      const fixedContent = fixContent(content);
-
-      if (content !== fixedContent) {
-        fs.writeFileSync(filePath, fixedContent, "utf8");
-        console.log(`✅ Korrigiert: ${file}`);
-      } else {
-        console.log(`👌 Keine Änderungen: ${file}`);
-      }
-    }
-  });
 }
 
 checkFiles();

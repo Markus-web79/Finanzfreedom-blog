3bb008bfc89970f93af24ee883d1c580284f149f
Markus-web79 <markushabermann@web.de>
2025-10-20 23:18:46 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import path from "path";
 import matter from "gray-matter";
 import Link from "next/link";
 import Header from "../components/Header";
-import NewsletterFooter from "../components/NewsletterFooterFixed";
+import NewsletterFooterFixed from "../components/NewsletterFooterFixed.js";
 
 
 // 📄 Artikel aus content/-Ordner laden

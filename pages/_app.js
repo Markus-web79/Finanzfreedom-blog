c1b128e73260be73e88489eae248ff968851bced
Markus-web79 <markushabermann@web.de>
2025-10-20 23:05:33 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
-import "../styles/globals.css";
+// 🔹 Lädt die globalen Styles (z. B. Home.css)
+import "../styles/Home.css";
+
+// 🔹 Optional: Wenn du später noch eine weitere globale CSS-Datei hast
+// import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;

69ac30e513d15b4532c3fe1aa88a1a5471f5199f
Markus-web79 <markushabermann@web.de>
2025-10-21 12:58:04 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
 import "../styles/Home.css";
 
-// 🔧 Diese Datei sorgt dafür, dass dein globales CSS (Home.css) korrekt geladen wird
-//    und für alle Seiten verfügbar ist (index.js, [slug].js etc.)
-
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }

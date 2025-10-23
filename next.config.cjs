3380a3108f28a2cdb15c980cec0d3b99f42c8eb1
Markus-web79 <markushabermann@web.de>
2025-10-23 10:44:03 +0200

    Update next.config.cjs

--- next.config.cjs
+++ next.config.cjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'out', // hierhin schreibt Next.js die statischen Dateien
-  trailingSlash: true,
-  images: { unoptimized: true },
+  output: 'export', // Wichtig: sorgt für statischen Export
+  images: {
+    unoptimized: true, // verhindert Fehler bei Vercel static export
+  },
+  trailingSlash: true, // sorgt für saubere URLs (/seite/)
+  reactStrictMode: true,
 };
 
 export default nextConfig;

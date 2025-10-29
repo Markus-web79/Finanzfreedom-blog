bfd13b0af77ae82374b03a4ab15fc49e3f475750
Markus-web79 <markushabermann@web.de>
2025-10-30 00:16:57 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,19 @@
-import Link from "next/link";
-import styles from "../styles/Header.module.css";
+// components/Header.js
+import Link from 'next/link';
+import styles from '../styles/Header.module.css';
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.container}>
-        <Link href="/" className={styles.logo}>
-          Finanz<span>Freedom</span>
-        </Link>
-
-        <nav className={styles.nav}>
-          <Link href="/#artikel">Artikel</Link>
-          <Link href="/vergleiche">Vergleiche</Link>
-          <Link href="/ratgeber">Ratgeber</Link>
-          <Link href="/ueber-uns">Über uns</Link>
-          <Link href="/kontakt">Kontakt</Link>
-        </nav>
+      <div className={styles.logo}>
+        <Link href="/">FinanzFreedom</Link>
       </div>
+      <nav className={styles.nav}>
+        <Link href="/">Start</Link>
+        <Link href="/kategorien">Themen</Link>
+        <Link href="/ueber-uns">Über uns</Link>
+        <Link href="/kontakt">Kontakt</Link>
+      </nav>
     </header>
   );
 }

31f0295bc487efb7976d1ceead0765001b0e8ee5
Markus-web79 <markushabermann@web.de>
2025-10-29 23:28:16 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,22 @@
-import Link from "next/link";
-import styles from "../styles/Header.module.css";
+import Link from 'next/link';
+import styles from '../styles/Header.module.css';
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.container}>
-        <div className={styles.logo}>
-          <Link href="/">Finanz<span>Freedom</span></Link>
-        </div>
-        <nav className={styles.nav}>
-          <Link href="/">Startseite</Link>
-          <Link href="/etfs">ETFs</Link>
-          <Link href="/geld-anlegen">Geld anlegen</Link>
-          <Link href="/versicherungen">Versicherungen</Link>
-          <Link href="/ueber-uns">Über uns</Link>
-        </nav>
+      <div className={styles.logo}>
+        <Link href="/">
+          Finanz<span>Freedom</span>
+        </Link>
       </div>
+
+      <nav className={styles.nav}>
+        <Link href="/">Startseite</Link>
+        <Link href="/etfs">ETFs</Link>
+        <Link href="/geld-anlegen">Geld anlegen</Link>
+        <Link href="/versicherungen">Versicherungen</Link>
+        <Link href="/kontakt">Kontakt</Link>
+      </nav>
     </header>
   );
 }

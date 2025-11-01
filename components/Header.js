213aa91405cf410c338074f3352ff8adeafba5d9
Markus-web79 <markushabermann@web.de>
2025-11-01 01:02:08 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,31 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from '../styles/Header.module.css';
 
 export default function Header() {
+  const router = useRouter();
+
   return (
     <header className={styles.header}>
+      {/* Logo */}
       <div className={styles.logo}>
-        <Link href="/">Finanz<span>Freedom</span></Link>
+        Finanz<span>Freedom</span>
       </div>
+
+      {/* Navigation */}
       <nav className={styles.nav}>
-        <Link href="/">Startseite</Link>
-        <Link href="/blog">Blog</Link>
-        <Link href="/ueber-uns">Über uns</Link>
-        <Link href="/kontakt">Kontakt</Link>
+        <Link href="/" className={router.pathname === '/' ? styles.active : ''}>
+          Startseite
+        </Link>
+        <Link href="/willkommen" className={router.pathname === '/willkommen' ? styles.active : ''}>
+          Willkommen
+        </Link>
+        <Link href="/ueber-uns" className={router.pathname === '/ueber-uns' ? styles.active : ''}>
+          Über uns
+        </Link>
+        <Link href="/kontakt" className={router.pathname === '/kontakt' ? styles.active : ''}>
+          Kontakt
+        </Link>
       </nav>
     </header>
   );

6de1221b8dbc00849c173fcce267f350e520e527
Markus-web79 <markushabermann@web.de>
2025-11-06 22:33:06 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home({ posts }) {
                 <div key={post.slug} className={styles.card}>
                   <h3>{post.title}</h3>
                   <p>{post.description}</p>
-                  <Link href={`/${post.slug}`} className={styles.link}>
+                 <Link href={`/${post.category}/${post.slug}/`}>Weiterlesen →</Link>
                     Weiterlesen →
                   </Link>
                 </div>

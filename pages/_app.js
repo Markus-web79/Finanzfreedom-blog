6f5f4cb29184a8a9ed108c0cc978eadf2416d1f3
Markus-web79 <markushabermann@web.de>
2025-11-07 19:49:05 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,11 @@
 import "../styles/globals.css";
 import Header from "../components/Header";
-import fs from "fs";
-import path from "path";
 
-function MyApp({ Component, pageProps, categories }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Header categories={categories} />
+      <Header />
       <Component {...pageProps} />
     </>
   );
 }
-
-MyApp.getInitialProps = async () => {
-  const contentDir = path.join(process.cwd(), "content");
-  let categories = [];
-
-  try {
-    categories = fs
-      .readdirSync(contentDir)
-      .filter((dir) => fs.statSync(path.join(contentDir, dir)).isDirectory());
-  } catch (err) {
-    console.error("⚠️ Konnte Kategorien nicht laden:", err);
-  }
-
-  return { categories };
-};
-
-export default MyApp;

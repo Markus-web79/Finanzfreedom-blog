68ce1cd2afbd956225f94a3dc44ed199e1f57e8e
Markus-web79 <markushabermann@web.de>
2025-11-07 19:36:35 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,11 @@
-import '../styles/globals.css';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import "../styles/globals.css";
+import Header from "../components/Header";
 
-// Falls du später wieder Inter-Schriftarten nutzen willst,
-// einfach diese zwei Zeilen aktivieren und vorher installieren:
-// import '@fontsource/inter/400.css';
-// import '@fontsource/inter/600.css';
-
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Header />
-      <main>
-        <Component {...pageProps} />
-      </main>
-      <Footer />
+      <Header categories={pageProps.categories || []} />
+      <Component {...pageProps} />
     </>
   );
 }
-
-export default MyApp;

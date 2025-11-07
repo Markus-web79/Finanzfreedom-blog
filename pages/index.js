4cb8a1deb4567154f003f3bcc51773432c8f7175
Markus-web79 <markushabermann@web.de>
2025-11-08 00:22:43 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,3 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-import Link from "next/link";
-import Head from "next/head";
-import Hero from "../components/Hero";
-import styles from "../styles/Home.module.css";
-
-export default function Home({ posts = {} }) {
-  if (!posts || Object.keys(posts).length === 0) {
-    return (
-      <div style={{ color: "#fff", textAlign: "center", padding: "4rem" }}>
-        <h1>📉 Keine Artikel gefunden</h1>
-        <p>Bitte überprüfe die Verbindung oder den Content-Ordner.</p>
-      </div>
-    );
-  }
-
-  return (
-    <>
-      <Head>
-        <title>FinanzFreedom – Dein Weg zur finanziellen Freiheit</title>
-        <meta
-          name="description"
-          content="Lerne, wie dein Geld für dich arbeiten lässt – einfach, ehrlich und unabhängig. Finanzielle Freiheit beginnt mit Wissen."
-        />
-      </Head>
-
-      <Hero />
-
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 

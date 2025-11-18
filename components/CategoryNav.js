2cfccca7ab75b94440a69ccff5d0a8a282732918
Markus-web79 <markushabermann@web.de>
2025-11-18 20:21:22 +0100

    Update CategoryNav.js

--- components/CategoryNav.js
+++ components/CategoryNav.js
@@ -1,17 +1,23 @@
 import Link from "next/link";
 import styles from "../styles/CategoryNav.module.css";
-import categories from "../config/categories.json";
+import { CATEGORY_CONFIG } from "../config/categoriesConfig";
 
 export default function CategoryNav({ active }) {
+  // Kategorien aus der Config extrahieren
+  const categories = Object.values(CATEGORY_CONFIG).map(cat => ({
+    slug: cat.slug,
+    label: cat.label
+  }));
+
   return (
     <nav className={styles.nav}>
-      {Object.values(categories).map((cat) => (
-        <Link
-          key={cat.slug}
-          href={`/category/${cat.slug}`}
+      {categories.map(cat => (
+        <Link 
+          key={cat.slug} 
+          href={`/category/${cat.slug}`} 
           className={active === cat.slug ? styles.active : ""}
         >
-          {cat.shortLabel || cat.label}
+          {cat.label}
         </Link>
       ))}
     </nav>

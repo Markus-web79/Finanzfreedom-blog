5b412e8d9963a26319a43219d464b7b004bf8d90
Markus-web79 <markushabermann@web.de>
2025-11-21 16:41:04 +0100

    Update index.js

--- pages/[category]/index.js
+++ pages/[category]/index.js
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import Link from "next/link";
-import CATEGORY_CONFIG from "../../config/categoriesConfig";
+import CATEGORY_CONFIG from "../../config/categoryConfig";
 export default function CategoryPage({ categoryInfo, articles }) {
   return (
     <div style={{ padding: "2rem" }}>

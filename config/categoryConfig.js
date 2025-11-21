ebe2d868325ae5d8666970673fbb70097057976e
Markus-web79 <markushabermann@web.de>
2025-11-21 16:23:31 +0100

    Update and rename categoriesConfig.js to categoryConfig.js

---
+++ config/categoryConfig.js
@@ -0,0 +1,19 @@
+const CATEGORY_CONFIG = {
+  investieren: {
+    slug: "investieren",
+    label: "Investieren",
+    heroSubtitle: "Lerne clever zu investieren",
+  },
+  versicherungen: {
+    slug: "versicherungen",
+    label: "Versicherungen",
+    heroSubtitle: "Schütze dein Geld",
+  },
+  geld_verdienen: {
+    slug: "geld_verdienen",
+    label: "Geld verdienen",
+    heroSubtitle: "Verdiene mehr Einkommen",
+  },
+};
+
+export default CATEGORY_CONFIG;

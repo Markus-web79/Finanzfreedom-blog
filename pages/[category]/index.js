2ee3bd59931963aeebd3cd523d9c3c441c1fe025
Markus-web79 <markushabermann@web.de>
2025-11-23 22:52:40 +0100

    Update index.js

--- pages/[category]/index.js
+++ pages/[category]/index.js
@@ -2,7 +2,8 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import Link from "next/link";
-import categoryConfig from '../../config/categoryConfig.js';
+-import categoryConfig from '../../../config/categoryConfig';
++import categoryConfig from '../../../config/categoryConfig.js';
 
 export default function CategoryPage({ categoryInfo, articles }) {
   return (

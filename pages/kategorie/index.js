7001f619d6e40c583dd4f71a6ecefda9c5571c39
Markus-web79 <markushabermann@web.de>
2025-11-24 20:08:27 +0100

    Create index.js

---
+++ pages/kategorie/index.js
@@ -0,0 +1,20 @@
+import CATEGORY_CONFIG from "../../config/categoryConfig";
+
+export default function KategorieOverview() {
+  const categories = Object.values(CATEGORY_CONFIG);
+
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Kategorien</h1>
+      <p>Wähle eine Kategorie aus:</p>
+
+      <ul>
+        {categories.map(cat => (
+          <li key={cat.slug}>
+            <a href={`/kategorie/${cat.slug}`}>{cat.label}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}

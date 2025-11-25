f3f62ee5e27dd8e19c7b2f5b552e542b81a29ddc
Markus-web79 <markushabermann@web.de>
2025-11-25 17:46:13 +0100

    Create index.js

---
+++ pages/kategorie/index.js
@@ -0,0 +1,8 @@
+export default function KategorieHome() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Kategorien</h1>
+      <p>Hier erscheinen die Kategorien – Basis-Seite für Next.js Build.</p>
+    </div>
+  );
+}

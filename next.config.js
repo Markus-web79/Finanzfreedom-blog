ea9bde0899be5cd97113138289bbbb97fb3655c0
Markus-web79 <markushabermann@web.de>
2025-12-07 23:10:47 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // Wichtig für statischen Export (SSG)
   output: "export",
-
   images: {
     unoptimized: true,
   },
-
   trailingSlash: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;

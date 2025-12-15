4c0d59348b6b3db7b6c3cfb2456877cf81121dc2
Markus-web79 <markushabermann@web.de>
2025-12-15 23:40:09 +0100

    Update content.ts

--- lib/content.ts
+++ lib/content.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-import { affiliates } from "./affiliates";
+import { AFFILIATES } from "./affiliates";
 
 export type Post = {
   title: string;

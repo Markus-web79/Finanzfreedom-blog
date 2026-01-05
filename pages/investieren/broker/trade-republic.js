00dceae7f3036754533182f73741848936ab9c6f
Markus-web79 <markushabermann@web.de>
2026-01-05 22:04:42 +0100

    Update trade-republic.js

--- pages/investieren/broker/trade-republic.js
+++ pages/investieren/broker/trade-republic.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
-import styles from "@/styles/Broker.module.css";
+import styles from "../../../styles/Broker.module.css";
 
 export default function TradeRepublic() {
   return (

7f74b18b00fd1f72a56cfa0ad64c47e28ded4ddf
Markus-web79 <markushabermann@web.de>
2026-01-06 20:34:58 +0100

    Create BackLink.js

---
+++ components/BackLink.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import styles from "../styles/BackLink.module.css";
+
+export default function BackLink({ href, label }) {
+  return (
+    <div className={styles.back}>
+      <Link href={href}>
+        ← {label}
+      </Link>
+    </div>
+  );
+}

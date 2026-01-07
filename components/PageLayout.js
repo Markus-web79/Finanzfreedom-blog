5ac27df4542a908c9cfeb71ffef5f4985ddd5943
Markus-web79 <markushabermann@web.de>
2026-01-07 11:58:36 +0100

    Update PageLayout.js

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -1,31 +1,10 @@
-import { useRouter } from "next/router";
 import BackLink from "./BackLink";
 
 export default function PageLayout({ children }) {
-  const router = useRouter();
-
-  let backHref = "/";
-  let backLabel = "← Zur Startseite";
-
-  if (router.pathname.startsWith("/investieren")) {
-    backHref = "/investieren";
-    backLabel = "← Zurück zu Investieren";
-  }
-
-  if (router.pathname.startsWith("/versicherungen")) {
-    backHref = "/versicherungen";
-    backLabel = "← Zurück zu Versicherungen";
-  }
-
-  if (router.pathname.startsWith("/wissen")) {
-    backHref = "/wissen";
-    backLabel = "← Zurück zu Wissen";
-  }
-
   return (
     <>
-      <BackLink href={backHref} label={backLabel} />
-      {children}
+      <BackLink />
+      <main>{children}</main>
     </>
   );
 }

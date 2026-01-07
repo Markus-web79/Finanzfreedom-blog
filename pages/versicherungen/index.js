94f9fc2e8856e048cba4e736386ff1802df70056
Markus-web79 <markushabermann@web.de>
2026-01-07 10:29:12 +0000

    Update index.js

--- pages/versicherungen/index.js
+++ pages/versicherungen/index.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import BackLink from "../../components/BackLink";
 import styles from "../../styles/CategoryPage.module.css";
-
+import PageLayout from "../../components/PageLayout";
 export default function Versicherungen() {
-  return (
+return (
+  <PageLayout>
     <div className={styles.page}>
-      <BackLink href="/" label="Zur Startseite" />
+      ...
 
       <h1>Versicherungen – sinnvoll & verständlich</h1>
       <p className={styles.intro}>
@@ -49,3 +50,6 @@ export default function Versicherungen() {
     </div>
   );
 }
+    </div>
+  </PageLayout>
+);

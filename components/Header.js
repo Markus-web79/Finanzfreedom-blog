0123648372d0d6e527c005859828a7994d974de8
Markus-web79 <markushabermann@web.de>
2026-01-10 17:39:04 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -5,10 +5,7 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.inner}>
-        <nav className={styles.nav}>
-          <Link href="/">Startseite</Link>
-          <Link href="/blog">Blog</Link>
-        </nav>
+
       </div>
     </header>
   );

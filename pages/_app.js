5dcf5877ef3ebd2e23e972d5cb96ce02a0628a16
Markus-web79 <markushabermann@web.de>
2026-01-21 17:55:10 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,30 @@
 import Head from "next/head";
+import Script from "next/script";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* Google Analytics */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-Y3NPFLQ9NK"
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-Y3NPFLQ9NK', {
-                anonymize_ip: true
-              });
-            `,
-          }}
-        />
+        <title>FinanzFreedom</title>
       </Head>
+
+      {/* Google Analytics – offizieller Next.js Weg */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-Y3NPFLQ9NK"
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-Y3NPFLQ9NK', {
+            anonymize_ip: true
+          });
+        `}
+      </Script>
 
       <Component {...pageProps} />
     </>

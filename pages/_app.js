e8fc89dfd8c99a2311c6ac3873e9636d2c3f56cc
Markus-web79 <markushabermann@web.de>
2026-01-21 17:26:16 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,30 @@
+import Head from "next/head";
 import "../styles/globals.css";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Header />
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-Y3NPFLQ9NK"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-Y3NPFLQ9NK', {
+                anonymize_ip: true
+              });
+            `,
+          }}
+        />
+      </Head>
+
       <Component {...pageProps} />
-      <Footer />
     </>
   );
 }
-
-export default MyApp;

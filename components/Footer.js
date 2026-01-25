5afeb30824550330c71828082b35b41e4bbb83b0
Markus-web79 <markushabermann@web.de>
2026-01-25 20:56:13 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,13 @@
-import styles from "../styles/Footer.module.css";
 import Link from "next/link";
+import styles from "../styles/Footer.module.css";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.inner}>
-        <span>© {new Date().getFullYear()} FinanzFreedom</span>
+        <span className={styles.copy}>
+          © {new Date().getFullYear()} FinanzFreedom
+        </span>
 
         <nav className={styles.links}>
           <Link href="/ueber-uns">Über uns</Link>

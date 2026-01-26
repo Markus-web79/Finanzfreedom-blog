b82d377c5ccdf60c1dd30847dd4815bee9c953f8
Markus-web79 <markushabermann@web.de>
2026-01-26 13:10:18 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,17 @@
 // pages/_document.js
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="de">
       <Head>
-        {/* impact.com site verification */}
+        {/* impact.com Site Verification */}
         <meta
           name="impact-site-verification"
-          value="e4a2b0e8-88b4-4d26-9895-28e4e9a4de7e"
+          value="e4a2b0e8-884b-4d26-9895-28e4e9a4de7e"
         />
 
-        {/* (optional) AWIN verification – kannst du drin lassen */}
-        <meta
-          name="verification"
-          content="c7f1a5acf5b563516e05e2f401e50a8"
-        />
-
-        {/* TEMP: Seite nicht indexieren */}
+        {/* TEMP: solange wir prüfen, nicht indexieren */}
         <meta name="robots" content="noindex, nofollow" />
       </Head>
       <body>

352aa8551bfdb6e0cd93f56eeb4c1b7a71b9db70
Markus-web79 <markushabermann@web.de>
2026-02-12 19:30:04 +0100

    Update ETFSignup.tsx

--- components/ETFSignup.tsx
+++ components/ETFSignup.tsx
@@ -1,16 +1,16 @@
 export default function ETFSignup() {
   return (
-    <section className="etfSignup">
+    <section className="etfSignup" style={{ marginTop: "4rem" }}>
       <iframe
         src="https://9e0f1216.sibforms.com/serve/MUIFACuRiADRUssR6UNDEVB7MgiCd9FuyB698G-aWqi89jdoeZgzhaRz-FI7nZ1AYB3WFLXhOjjrkaT5d5iLYlAXWMXjUuKKRWTe_Uc_L8sx0aEr4q_AefgabBlRiqAWTlkuOjqw2om2DgMmHNbamrFV5RH9iofDTJF73U2RcioAiVQvbFvtdJuJb6MItpnjdOU0NCD_P-mtA5B7ng=="
+        width="100%"
+        height="380"
         style={{
-          width: "100%",
-          height: "360px",
           border: "0",
-          marginTop: "3rem",
+          display: "block",
+          maxWidth: "100%",
         }}
-        scrolling="auto"
-        allowFullScreen
+        loading="lazy"
       />
     </section>
   );

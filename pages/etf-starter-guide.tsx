a665f004364f425ec2ff26d579b92436897bcd97
Markus-web79 <markushabermann@web.de>
2026-02-25 22:07:26 +0100

    Create etf-starter-guide.tsx

---
+++ pages/etf-starter-guide.tsx
@@ -0,0 +1,24 @@
+export default function ETFStarterGuide() {
+  return (
+    <div style={{ padding: "60px 20px", maxWidth: "900px", margin: "0 auto", textAlign: "center" }}>
+      
+      <h1 style={{ fontSize: "42px", marginBottom: "20px" }}>
+        ETF Starter Guide 2026
+      </h1>
+
+      <p style={{ fontSize: "18px", marginBottom: "40px", color: "#555" }}>
+        Verständlich erklärt. Ohne Fachchinesisch. Kostenlos als PDF.
+      </p>
+
+      <div style={{ width: "100%", maxWidth: "600px", margin: "0 auto" }}>
+        <iframe
+          src="https://9e0f1216.sibforms.com/serve/MUIFACuRiADRUssR6UNDEVB7MgiCd9FuyB698G-aWqi89jdoeZgzhaRz-FI7nZ1AYB3WFLXhOjjrkaT5d5iLYlAXWMXjUuKKRWTe_Uc_L8sx0aEr4q_AefgabBlRiqAWTlkuOjqw2om2DgMmHNbamrFV5RH9iofDTJF73U2RcioAiVQvbFvtdJuJb6MItpnjdOU0NCD_P-mtA5B7ng=="
+          width="100%"
+          height="600"
+          style={{ border: "none" }}
+        ></iframe>
+      </div>
+
+    </div>
+  );
+}

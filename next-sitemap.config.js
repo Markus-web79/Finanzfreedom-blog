9eed7fa313341d49b47914c9a49178c01c4a2b29
Markus-web79 <markushabermann@web.de>
2026-02-26 19:02:52 +0100

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,38 +1,9 @@
-const fs = require("fs");
-const path = require("path");
-
-const getAllMarkdownPaths = (dir, baseUrl = "") => {
-  const entries = fs.readdirSync(dir, { withFileTypes: true });
-
-  return entries.flatMap((entry) => {
-    const fullPath = path.join(dir, entry.name);
-
-    if (entry.isDirectory()) {
-      return getAllMarkdownPaths(fullPath, `${baseUrl}/${entry.name}`);
-    }
-
-    if (entry.name.endsWith(".md")) {
-      const slug = entry.name.replace(".md", "");
-      return `${baseUrl}/${slug}`;
-    }
-
-    return [];
-  });
-};
-
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://finanzfreedom.de",
   generateRobotsTxt: true,
   sitemapSize: 7000,
-
-  additionalPaths: async (config) => {
-    const contentPath = path.join(process.cwd(), "content");
-    const markdownRoutes = getAllMarkdownPaths(contentPath);
-
-    return Promise.all(
-      markdownRoutes.map((route) =>
-        config.transform(config, route)
-      )
-    );
-  },
+  changefreq: "weekly",
+  priority: 0.7,
+  sourceDir: ".next",
 };

fd9c68c90a77a50b1e3157005220f3d805b41eba
Markus-web79 <markushabermann@web.de>
2026-02-26 18:42:50 +0100

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,16 @@ module.exports = {
   siteUrl: "https://finanzfreedom.de",
   generateRobotsTxt: true,
   sitemapSize: 7000,
+  changefreq: "weekly",
+  priority: 0.7,
+
+  additionalPaths: async (config) => [
+    await config.transform(config, "/"),
+    await config.transform(config, "/etfs"),
+    await config.transform(config, "/investieren"),
+    await config.transform(config, "/versicherungen"),
+    await config.transform(config, "/sparen"),
+    await config.transform(config, "/wissen"),
+    await config.transform(config, "/broker"),
+  ],
 };

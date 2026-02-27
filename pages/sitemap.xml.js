df2c24bcfbd8f6183e46cd4f0b04184f2fbc9c3c
Markus-web79 <markushabermann@web.de>
2026-02-27 20:05:16 +0100

    Update sitemap.xml.js

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,4 +1,6 @@
-export async function getServerSideProps({ res }) {
+import type { GetServerSideProps } from "next";
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   const siteUrl = "https://www.finanzfreedom.de";
 
   const pages = [
@@ -8,29 +10,33 @@ export async function getServerSideProps({ res }) {
     "versicherungen",
     "wissen",
     "tools",
-    "broker",
+    "broker"
   ];
 
-  const urls = pages.map(
-    (page) => `
-    <url>
-      <loc>${siteUrl}/${page}</loc>
-      <changefreq>daily</changefreq>
-      <priority>0.7</priority>
-    </url>`
-  );
+  const urls = pages
+    .map((page) => {
+      const path = page ? `/${page}` : "";
+      return `
+        <url>
+          <loc>${siteUrl}${path}</loc>
+          <changefreq>daily</changefreq>
+          <priority>0.7</priority>
+        </url>`;
+    })
+    .join("");
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urls.join("")}
-  </urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+</urlset>`;
 
-  res.setHeader("Content-Type", "text/xml");
+  res.setHeader("Content-Type", "application/xml");
+  res.setHeader("Cache-Control", "public, s-maxage=3600, stale-while-revalidate=86400");
   res.write(sitemap);
   res.end();
 
   return { props: {} };
-}
+};
 
 export default function Sitemap() {
   return null;

965a0eacffcb63c924ebbfdbde4f221c3bd0fe8b
Markus-web79 <markushabermann@web.de>
2026-03-03 17:29:35 +0100

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://www.finanzfreedom.de',
-  generateRobotsTxt: false,
+  generateRobotsTxt: true,
   sitemapSize: 5000,
   changefreq: 'daily',
   priority: 0.7,
-  exclude: ['/404', '/500']
+
+  additionalPaths: async (config) => {
+    const contentDir = path.join(process.cwd(), 'content');
+    const files = fs.readdirSync(contentDir);
+
+    const dynamicPaths = files
+      .filter((file) => file.endsWith('.md'))
+      .map((file) => {
+        const slug = file.replace('.md', '');
+        return {
+          loc: `/blog/${slug}`,
+          changefreq: 'daily',
+          priority: 0.7,
+        };
+      });
+
+    return dynamicPaths;
+  },
 };

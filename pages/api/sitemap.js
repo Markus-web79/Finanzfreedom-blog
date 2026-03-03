01578b85fd2cb11bed8d22d467099934fa0c4fd8
Markus-web79 <markushabermann@web.de>
2026-03-03 19:21:47 +0100

    Update sitemap.js

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -8,23 +8,23 @@ export default function handler(req, res) {
     "versicherungen",
     "wissen",
     "tools",
-    "broker"
+    "broker",
   ];
+
+  const urls = routes.map((route) => {
+    return `
+    <url>
+      <loc>${baseUrl}${route ? `/${route}` : ""}</loc>
+      <changefreq>daily</changefreq>
+      <priority>0.7</priority>
+    </url>`;
+  });
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${routes
-  .map(
-    (route) => `
-  <url>
-    <loc>${baseUrl}${route ? `/${route}` : ""}</loc>
-    <changefreq>daily</changefreq>
-    <priority>0.7</priority>
-  </url>`
-  )
-  .join("")}
+${urls.join("")}
 </urlset>`;
 
-  res.setHeader("Content-Type", "application/xml");
+  res.setHeader("Content-Type", "text/xml");
   res.status(200).send(xml);
 }

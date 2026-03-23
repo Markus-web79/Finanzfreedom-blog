16118918f228c0c512c9c45c30d9b440960be2c6
Markus-web79 <markushabermann@web.de>
2026-03-23 20:22:14 +0100

    Update verify.js

--- pages/verify.js
+++ pages/verify.js
@@ -1,4 +1,10 @@
-<div style={{padding: "40px", background: "white", color: "black"}}>
-  <h1>Verification</h1>
-  <p>Impact-Site-Verification: 2382a9c4-470f-40ef-b53a-eee4bb3299e8</p>
-</div>
+export default function Verify() {
+  return (
+    <div style={{ padding: "40px", background: "white", color: "black" }}>
+      <h1>Verification</h1>
+      <p>
+        Impact-Site-Verification: 2382a9c4-470f-40ef-b53a-eee4bb3299e8
+      </p>
+    </div>
+  );
+}

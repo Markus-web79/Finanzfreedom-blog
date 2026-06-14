df3af18ef6ed6cccb2910d9973511348649a38c9
Markus-web79 <markushabermann@web.de>
2026-06-14 22:57:48 +0200

    Update index.js

--- pages/gas/index.js
+++ pages/gas/index.js
@@ -1,5 +1,8 @@
 import Link from "next/link";
 
+const GAS_AFFILIATE_LINK =
+  "https://www.awin1.com/cread.php?awinmid=14797&awinaffid=2757918&ued=https%3A%2F%2Fwww.verivox.de%2Fgasvergleich%2F";
+
 export default function GasIndex() {
   return (
     <main style={styles.page}>
@@ -16,7 +19,7 @@ export default function GasIndex() {
         </p>
 
         <a
-          href="https://www.verivox.de/gasvergleich/"
+          href={GAS_AFFILIATE_LINK}
           target="_blank"
           rel="nofollow sponsored noopener noreferrer"
           style={styles.heroButton}
@@ -94,7 +97,7 @@ export default function GasIndex() {
         </p>
 
         <a
-          href="https://www.verivox.de/gasvergleich/"
+          href={GAS_AFFILIATE_LINK}
           target="_blank"
           rel="nofollow sponsored noopener noreferrer"
           style={styles.ctaButton}
